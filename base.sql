86ef1edfdaebaeb84790086ad04a537839c32e02
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2014-04-26 18:26:27 +0200

    Erreur corrigé base.sql

--- base.sql
+++ base.sql
@@ -1,82 +1,56 @@
 DROP TABLE IF EXISTS Vehicule;
 DROP TABLE IF EXISTS Transport;
-DROP TABLE IF EXISTS Ville;
 DROP TABLE IF EXISTS Suggestion;
 DROP TABLE IF EXISTS Disponibilite;
 DROP TABLE IF EXISTS Photo;
-DROP TABLE IF EXISTS Adresse;
-DROP TABLE IF EXISTS Logement;
-DROP TABLE IF EXISTS Compte;
 DROP TABLE IF EXISTS Prestation;
-DROP TABLE IF EXISTS Reservation;
-DROP TABLE IF EXISTS Reduction;
+
 DROP TABLE IF EXISTS Reduction_duree;
 DROP TABLE IF EXISTS Reduction_periode;
-DROP TABLE IF EXISTS Personne;
+DROP TABLE IF EXISTS Reduction;
+
 DROP TABLE IF EXISTS Facture;
 
-
-CREATE TABLE Vehicule(
-       idDisponible serial,
-       heure date,  
-       idReservation integer,
-       idTransport varchar(64),
-       PRIMARY KEY(idDisponible),
-       FOREIGN KEY(idReservation) REFERENCES Reservation(idReservation),
-       FOREIGN KEY(idTransport) REFERENCES Transport(ville)
-);       
-
-
-CREATE TABLE Transport(
-       ville varchar(64),
-       nb_vehicule_libre integer,
-       prix money,
-       PRIMARY KEY(ville),
-       FOREIGN KEY(ville) REFERENCES Ville(ville)
-);
-
+DROP TABLE IF EXISTS Reservation;
+DROP TABLE IF EXISTS Logement;
+DROP TABLE IF EXISTS Compte;
+DROP TABLE IF EXISTS Personne;
+DROP TABLE IF EXISTS Adresse;
+DROP TABLE IF EXISTS Ville;
 
 CREATE TABLE Ville(
        ville varchar(64), 
        PRIMARY KEY(ville)
 );
 
-
-CREATE TABLE Suggestion(
-       idSuggestion serial,
-       suggestion varchar(256),
-       ville varchar(64),
-       PRIMARY KEY(idSuggestion),
-       FOREIGN KEY(vill) REFERENCES Ville(ville)
-);
-
-
-CREATE TABLE Disponibilite(
-       idDisponibilite serial,
-       jour date,
-       idLogement integer,
-       PRIMARY KEY(idDisponibilite),
-       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
-);
-       
-
-CREATE TABLE Photo(
-       idPhoto serial,
-       image varchar(64),
-       idLogement integer,
-       PRIMARY KEY(idPhoto),
-       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
-);
-
-
 CREATE TABLE Adresse(
        idAdresse serial,	
        cp integer,
        numero integer,
        rue varchar(256),
-       PRIMARY KEY(idAdresse)
+       ville varchar(64),
+       PRIMARY KEY(idAdresse),
+       FOREIGN KEY(ville) REFERENCES Ville(ville)
 );
 
+CREATE TABLE Personne(
+       idPersonne integer,
+       nom varchar(32),
+       prenom varchar(32),
+       mail varchar(64),
+       idAdresse integer, 
+       login integer,    
+       PRIMARY KEY(idPersonne),
+       FOREIGN KEY(idAdresse) REFERENCES Adresse(idAdresse)
+);
+
+CREATE TABLE Compte(
+       login varchar(32),
+       password varchar(63),
+       idPersonne integer,
+       PRIMARY KEY(login), 
+       FOREIGN KEY(idPersonne) REFERENCES Personne(idPersonne)
+);
 
 CREATE TABLE Logement(
        idLogement serial,
@@ -92,15 +66,64 @@ CREATE TABLE Logement(
        FOREIGN KEY(login) REFERENCES Compte(login)
 );       	    
 
+CREATE TABLE Reservation(
+       idReservation serial,
+       date_reservation date,
+       debut date,
+       fin date,   
+       idPersonne integer, 
+       idLogement integer,
+       PRIMARY KEY(idReservation),
+       FOREIGN KEY(idPersonne) REFERENCES Personne(idPersonne),
+       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
+);	   
 
-CREATE TABLE Compte(
-       login varchar(32),
-       password varchar(63),
-       idPersonne integer,
-       PRIMARY KEY(login), 
-       FOREIGN KEY(idPersonne) REFERENCES Personne(idPersonne)
+CREATE TABLE Transport(
+       ville varchar(64),
+       nb_vehicule_libre integer,
+       prix money,
+       PRIMARY KEY(ville),
+       FOREIGN KEY(ville) REFERENCES Ville(ville)
 );
 
+-- ----------------------------------------------------------
+-- ----------------------------------------------------------
+-- ----------------------------------------------------------
+
+
+CREATE TABLE Vehicule(
+       idDisponible serial,
+       heure date,  
+       idReservation integer,
+       idTransport varchar(64),
+       PRIMARY KEY(idDisponible),
+       FOREIGN KEY(idReservation) REFERENCES Reservation(idReservation),
+       FOREIGN KEY(idTransport) REFERENCES Transport(ville)
+);       
+
+CREATE TABLE Suggestion(
+       idSuggestion serial,
+       suggestion varchar(256),
+       ville varchar(64),
+       PRIMARY KEY(idSuggestion),
+       FOREIGN KEY(ville) REFERENCES Ville(ville)
+);
+
+CREATE TABLE Disponibilite(
+       idDisponibilite serial,
+       jour date,
+       idLogement integer,
+       PRIMARY KEY(idDisponibilite),
+       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
+);
+       
+CREATE TABLE Photo(
+       idPhoto serial,
+       image varchar(64),
+       idLogement integer,
+       PRIMARY KEY(idPhoto),
+       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
+);
 
 CREATE TABLE Prestation(
        idPrestation serial,
@@ -108,29 +131,14 @@ CREATE TABLE Prestation(
        prix money,
        idLogement  integer,
        PRIMARY KEY(idPrestation),
-       FOREIGN KEY(idLogement) REFERENCES Personne(idLogement)
-  
+       FOREIGN KEY(idLogement) REFERENCES Logement(idLogement)
 );	
-
-
-CREATE TABLE Reservation(
-       idReservation serial,
-       date_reservation date,
-       debut date,
-       fin date,   
-       idPersonne integer, 
-       PRIMARY KEY(idReservation),
-       FOREIGN KEY(idPersonne) REFERENCES Personne(idPersonne),
-       FOREIGN KEY(idLogement) REFERENCES Personne(idLogement)
-);	       
-
 
 CREATE TABLE Reduction(
        idReduction serial,
        pourcentage integer,
        PRIMARY KEY(idReduction)
 );
-
 
 CREATE TABLE Reduction_duree(
        duree_min integer,
@@ -147,18 +155,6 @@ CREATE TABLE Reduction_periode(
 );
 
 
-CREATE TABLE Personne(
-       idPersonne integer,
-       nom varchar(32),
-       prenom varchar(32),
-       mail varchar(64),
-       idAdresse integer, 
-       login integer,    
-       PRIMARY KEY(idPersonne),
-       FOREIGN KEY(idAdresse) REFERENCES Adresse(idAdresse)
-);
-
-
 CREATE TABLE Facture(
        idFacture serial,
        dateFacture timestamp,		
@@ -167,7 +163,8 @@ CREATE TABLE Facture(
        idLoueur integer,
        idReservation integer,	
        PRIMARY KEY(idFacture),
-       FOREIGN KEY(idPayeur, idLoueur) REFERENCES Personne(idPersonne,idPersonne),
+       FOREIGN KEY(idPayeur) REFERENCES Personne(idPersonne),
+       FOREIGN KEY(idLoueur) REFERENCES Personne(idPersonne),
        FOREIGN KEY(idReservation) REFERENCES Reservation(idReservation)
 );
 	
